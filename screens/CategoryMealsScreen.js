68cebbb5e1f93a8e0cde1679df9a273369678650
Vudutha Venkata Surya Kiran <67712163+venkatasurya777@users.noreply.github.com>
2020-08-12 17:35:17 +0530

    Add files via upload

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
+
+const CategoryMealScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
+
+  const displayedMeals = availableMeals.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Meals found, maybe check your filters</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
+};
+
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
+};
+
+const styles = StyleSheet.create({
+  content: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
+export default CategoryMealScreen;
